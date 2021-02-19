09334512bacc50892fa8c1843ca9b625d2a04e6b
NicholasMukuha <38855817+NicholasMukuha@users.noreply.github.com>
2021-02-19 22:20:20 +0300

    Update myApp.js

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-
+app.get("/", (req,res) => {
+ if(process.env.MESSAGE_STYLE === "uppercase"){
+  res.json({"message":"HELLO JSON"}))}else{
+   res.json({"message":"json"})}
+}
 
 
 
